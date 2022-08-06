a17cf863e89a0c59593a6ba75a0743f09d812b50
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-08-06 19:08:29 +0530

    adding img in assets folder

--- public/src/App.js
+++ public/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter, Router, Route, Switch, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 const App = () => {
   return (
-    <>
-      <h1>Hello from app.js Public</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 export default App;
+
+// alt + shift + down arrow  copy the code and paste it in the file
